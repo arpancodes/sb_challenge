c5dc28f0846165bd1364f5541344f48bc3115f4d
Arpan Abhishek <arpanforbusiness@gmail.com>
2021-06-18 21:50:30 +0530

    sort by fee/weight to get top txns

--- index.js
+++ index.js
@@ -7,35 +7,74 @@ let block = {
   totalFee: 0,
 };
 
+const transactions = [];
+const MAX_WEIGHT = 4_000_000;
+
 fs.createReadStream("mempool.csv")
   .pipe(csv.parse())
   .on("error", (error) => console.error(error))
   .on("data", handleRow)
   .on("end", createBlockTxt);
 
+let position = 1;
 function handleRow(row) {
-  if (block.totalWeight > 4000000) return;
+  if (row[0] === "tx_id") return;
   const [tx_id, fee, weight, parents] = row;
-  const parentsArray = parents.split(";");
-  if (parents === "") {
-    block.transactions.push(tx_id);
-    block.totalFee += Number(fee);
-    block.totalWeight += Number(weight);
-  } else {
-    const isParentinBlock = parentsArray.every((parent) =>
-      block.transactions.includes(parent)
-    );
-    if (isParentinBlock) {
-      block.transactions.push(tx_id);
-      block.totalFee += Number(fee);
-      block.totalWeight += Number(weight);
-    }
-  }
+  transactions.push({
+    tx_id,
+    fee: Number(fee),
+    weight: Number(weight),
+    parents: parents === "" ? [] : parents.split(";"),
+    order: position,
+    feeWeight_ratio: Number(fee) / Number(weight),
+  });
+  position++;
 }
 
 function createBlockTxt() {
-  console.log(block);
+  transactions.sort((a, b) => (a.feeWeight_ratio > b.feeWeight_ratio ? -1 : 1));
+  const topTransactions = filterTopTransactions(transactions);
+  topTransactions.sort((a, b) => (a.order < b.order ? -1 : 1));
+  createBlock(topTransactions);
+  console.log(block.transactions.length);
   fs.writeFileSync("./block.txt", block.transactions.join("\n"), {
     encoding: "utf-8",
   });
+}
+
+function filterTopTransactions(transactions) {
+  const topTransactions = [];
+  let totalWeight = 0;
+  for (let i = 0; i < transactions.length; i++) {
+    topTransactions.push({ ...transactions[i] });
+    totalWeight += transactions[i].weight;
+    if (totalWeight > MAX_WEIGHT) {
+      break;
+    }
+  }
+  return topTransactions;
+}
+
+function createBlock(transactions) {
+  console.log(transactions.length);
+  transactions.forEach((txn) => {
+    addToBlock(txn);
+  });
+}
+
+function addToBlock(txn) {
+  if (txn.parents.length > 0) {
+    const isParentInBlock = txn.parents.every((parent) =>
+      block.transactions.includes(parent)
+    );
+    if (isParentInBlock) {
+      block.transactions.push(txn.tx_id);
+      block.totalFee += txn.fee;
+      block.totalWeight += txn.weight;
+    }
+  } else {
+    block.transactions.push(txn.tx_id);
+    block.totalFee += txn.fee;
+    block.totalWeight += txn.weight;
+  }
 }
